8260d95da920c8246e32ebc31d8e2a46276b2b56
AnzZmn <primitivezaman@gmail.com>
2026-07-13 02:34:07 +0530

    added modularity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, JetBrains_Mono, Manrope } from "next/font/google";
 import "./globals.css";
-import { Bricolage_Grotesque } from "next/font/google";
-import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Narvent",
